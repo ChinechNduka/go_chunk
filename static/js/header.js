f54717d7e56558d73f3133599c32d2400a7959cb
Gameznut <emmanuelapabiekun@gmail.com>
2022-08-15 02:51:53 +0100

    adjusting the ui test 26

--- static/js/header.js
+++ static/js/header.js
@@ -1,6 +1,8 @@
-const toggleBtn = document.querySelector('.nav--toggle-btn');
-const navLinks = document.querySelector('.nav-links')
+const toggleBtn = document.querySelector(".nav--toggle-btn");
+const navLinks = document.querySelector(".nav-links");
 
-toggleBtn.addEventListener('click', function() {
-    navLinks.classList.toggle('show');
-});
+if (toggleBtn) {
+  toggleBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+  });
+}
--- static/js/login.js
+++ static/js/login.js
@@ -2,29 +2,34 @@ const loginBtn = document.querySelector(".login-btn");
 const registerBtn = document.querySelector(".register-btn");
 const signinBtn = document.querySelector(".signin--btn");
 
-registerBtn.addEventListener("mouseover", () => {
-  registerBtn.style.backgroundColor = "#1F378C";
-});
-registerBtn.addEventListener("mouseout", () => {
-  registerBtn.style.backgroundColor = "#2645AF";
-});
-registerBtn.addEventListener("click", () => {
-  registerBtn.style.backgroundColor = "#5170D9";
-  
-});
+if (registerBtn) {
+  registerBtn.addEventListener("mouseover", () => {
+    registerBtn.style.backgroundColor = "#1F378C";
+  });
+  registerBtn.addEventListener("mouseout", () => {
+    registerBtn.style.backgroundColor = "#2645AF";
+  });
+  registerBtn.addEventListener("click", () => {
+    registerBtn.style.backgroundColor = "#5170D9";
+  });
+}
 
-signinBtn.addEventListener("mouseover", () => {
-  signinBtn.style.backgroundColor = "#957032";
-});
+if (signinBtn) {
+  signinBtn.addEventListener("mouseover", () => {
+    signinBtn.style.backgroundColor = "#957032";
+  });
 
-signinBtn.addEventListener("mouseout", () => {
-  signinBtn.style.backgroundColor = "#FFC056";
-});
+  signinBtn.addEventListener("mouseout", () => {
+    signinBtn.style.backgroundColor = "#FFC056";
+  });
 
-signinBtn.addEventListener("click", () => {
-  signinBtn.style.backgroundColor = "#FFD58E";
-});
+  signinBtn.addEventListener("click", () => {
+    signinBtn.style.backgroundColor = "#FFD58E";
+  });
+}
 
-loginBtn.addEventListener("click", () => {
-  loginBtn.style.opacity = "0.5";
-});
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    loginBtn.style.opacity = "0.5";
+  });
+}
